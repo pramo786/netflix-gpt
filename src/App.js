40b8436d6321cb887be50b411d5a832b9c7180a3
Pramod Yadav <pramo786>
2024-01-27 10:52:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-green-800">
+    Hello world!
+  </h1>
+
+  );
+}
+
+export default App;
